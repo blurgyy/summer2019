1b16d8d437b74e0ec7d4efa6560ffd6ae3249a3b
Blurgy <gaoyangzhang@outlook.com>
2019-09-05 23:32:55 +0800

    :wheelchair: reload configuration after each cycle

--- summer2019_mail/main.py
+++ summer2019_mail/main.py
@@ -12,13 +12,14 @@ def main():
     while(True):
         urls = spider.check();
         arts = [it.article(url) for url in urls];
-        updated = [it.mail(art) for art in arts]
+        updated = [it.mail(art) for art in arts];
         mails += updated;
         current_time = time.asctime(time.localtime(time.time()));
-        print(f"[{current_time}]: {len(updated)} updates posted, sending {len(mails)} items");
+        print(f"[{current_time}]: {len(updated)} update(s) posted, sending {len(mails)} item(s)");
         mails = [mail for mail in mails if(not mail.send(conf))];
         print();
         time.sleep(conf['cycle']);
+        conf = it.config.load();
 
 if(__name__ == "__main__"):
     main();
