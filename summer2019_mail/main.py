b066568a3a7df611bcd08c0b1e4fa82f0f6b7c73
Blurgy <gaoyangzhang@outlook.com>
2020-01-21 12:19:05 +0800

    :zap: excepted connection error

--- summer2019_mail/main.py
+++ summer2019_mail/main.py
@@ -3,6 +3,7 @@
 
 import it 
 import time 
+import requests 
 
 def main():
     conf = it.config.load();
@@ -10,13 +11,16 @@ def main():
     mails = [];
     # it.clear_cache(conf);
     while(True):
-        urls = spider.check();
-        arts = [it.article(url) for url in urls];
-        updated = [it.mail(art) for art in arts];
-        mails += updated;
         current_time = time.asctime(time.localtime(time.time()));
-        print(f"[{current_time}]: {len(updated)} update(s) posted, sending {len(mails)} item(s)");
-        mails = [mail for mail in mails if(not mail.send(conf))];
+        try:
+            urls = spider.check();
+            arts = [it.article(url) for url in urls];
+            updated = [it.mail(art) for art in arts];
+            mails += updated;
+            print(f"[{current_time}]: {len(updated)} update(s) posted, sending {len(mails)} item(s)");
+            mails = [mail for mail in mails if(not mail.send(conf))];
+        except requests.exceptions.ConnectionError as e:
+            print(f"[{current_time}]: exception occurred (connection error), retrying in {conf['cycle']} second(s)");
         time.sleep(conf['cycle']);
         conf = it.config.load();
         spider.load(conf);
